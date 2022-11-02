961bcfc8efd6b3eaa55e259d4c4208619806779a
bui tien <tien.bd1998@gmail.com>
2022-11-02 22:55:41 +0700

    innit project

---
+++ demogit/src/demojava/DemoGit.java
@@ -0,0 +1,13 @@
+package demojava;
+
+/* Welcome to project
+    @author: tienb
+    Date: 11/2/2022
+    Time: 10:46 PM
+    
+    ProjectName: demogit
+*/public class DemoGit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
