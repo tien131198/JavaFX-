bafb2abb71b67b80be0e2e61e41cbd951d4ef611
bui tien <tien.bd1998@gmail.com>
2022-11-02 23:25:55 +0700

    sua lan 2

--- demogit/src/demojava/DemoGit.java
+++ demogit/src/demojava/DemoGit.java
@@ -1,13 +1,16 @@
 package demojava;
 
+
 /* Welcome to project
     @author: tienb
     Date: 11/2/2022
     Time: 10:46 PM
     
     ProjectName: demogit
-*/public class DemoGit {
+*/
+public class DemoGit {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
+        System.out.println("hello cac ban");
     }
 }
